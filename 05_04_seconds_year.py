d36df6137e71270a287aa16a6c12ee27d6383e2c
ionjohn <77180739+jdaly67@users.noreply.github.com>
2024-07-19 22:48:29 -0400

    Add files via upload

---
+++ 05_04_seconds_year.py
@@ -0,0 +1,8 @@
+# Use the interpreter to calculate how many seconds are in a year.
+# Then write the code in this script file below this line.
+sec_min = 60
+min_hour = 60
+hour_day = 24
+days_year = 365.25
+sec_year = sec_min*min_hour*hour_day*days_year
+print("There are",sec_year,"seconds in a year.")
