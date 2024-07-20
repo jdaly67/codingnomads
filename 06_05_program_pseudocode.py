ddd023cade81b153a970cd621b360b00c0d3479f
ionjohn <77180739+jdaly67@users.noreply.github.com>
2024-07-20 19:11:07 -0400

    Add files via upload

---
+++ 06_05_program_pseudocode.py
@@ -0,0 +1,16 @@
+# Think about a task that you want to accomplish using programming
+# Maybe you want to automate something you do every day on your computer?
+# Break down some of the steps you can think of and write them in
+# this file as pseudocode.
+# You can come back and update this file when you've learned more.
+# You can also add real Python code after learning the necessary concepts.
+# For now, just practice breaking larger tasks into smaller steps
+# and writing out your thoughts in pseudocode.
+
+#daily spending tracker
+#what is your budget for the month?
+#record the day that you spent money on something
+#record how much you spent
+#what category (food, gas, entertainment, etc.) did you spend it on?
+#print how much of your budget you have left
+#graph your spending to date, colored by the spend category
