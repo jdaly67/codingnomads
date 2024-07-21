8d7ca86845e4e13366086de93659967d54c23a5d
ionjohn <77180739+jdaly67@users.noreply.github.com>
2024-07-21 14:15:03 -0400

    Add files via upload

---
+++ 07_04_multiple_improve.py
@@ -0,0 +1,7 @@
+# Improve the variable assignment shown below.
+# Why should you change it?
+
+#color, year = "red", 1900
+color = "red"
+year = 1900
+#the problem has mixed variables and is not easy to debug if you had an issue
